9982a931db526eda0413e3f23b51b351a0a8f48e
Zach Goldstein <zgoldstein@google.com>
2017-05-19 12:26:34 -0400

    Added chem lookup.

---
+++ src/chemicalLookup.js
@@ -0,0 +1,8 @@
+
+module.exports = {
+    foo: chemicalLookup,
+};
+
+function getProperty(chemical, property) {
+    return "Cocaine"
+}
--- src/index.js
+++ src/index.js
@@ -1,19 +1,19 @@
 'use strict';
-var Alexa = require('alexa-sdk');
-
+let Alexa = require('alexa-sdk');
+const chemicalLookup = require('chemicalLookup');
 //=========================================================================================================================================
 //TODO: The items below this comment need your attention.
 //=========================================================================================================================================
 
 //Replace with your app ID (OPTIONAL).  You can find this value at the top of your skill's page on http://developer.amazon.com.
 //Make sure to enclose your value in quotes, like this: var APP_ID = "amzn1.ask.skill.bb4045e6-b3e8-4133-b650-72923c5980f1";
-var APP_ID = 'amzn1.ask.skill.3fdf9436-7341-4264-9ae2-fa517c6cd13f';
+const APP_ID = 'amzn1.ask.skill.3fdf9436-7341-4264-9ae2-fa517c6cd13f';
 
-var SKILL_NAME = "Space Facts";
-var GET_FACT_MESSAGE = "Here's your fact: ";
-var HELP_MESSAGE = "You can say tell me a space fact, or, you can say exit... What can I help you with?";
-var HELP_REPROMPT = "What can I help you with?";
-var STOP_MESSAGE = "Goodbye!";
+const SKILL_NAME = "Space Facts";
+const GET_FACT_MESSAGE = "Here's your fact: ";
+const HELP_MESSAGE = "You can say tell me a space fact, or, you can say exit... What can I help you with?";
+const HELP_REPROMPT = "What can I help you with?";
+const STOP_MESSAGE = "Thank you for being safe! Goodbye!";
 
 //=========================================================================================================================================
 //TODO: Replace this data with your own.  You can find translations of this data at http://github.com/alexa/skill-sample-node-js-fact/data
@@ -38,13 +38,13 @@ var data = [
 //Editing anything below this line might break your skill.
 //=========================================================================================================================================
 exports.handler = function(event, context, callback) {
-    var alexa = Alexa.handler(event, context);
+    let alexa = Alexa.handler(event, context);
     alexa.APP_ID = APP_ID;
     alexa.registerHandlers(handlers);
     alexa.execute();
 };
 
-var handlers = {
+const handlers = {
     'LaunchRequest': function () {
         // this.emit('GetNewFactIntent');
         this.emit(':tell', 'hello lab');
@@ -57,9 +57,13 @@ var handlers = {
         // this.emit(':tellWithCard', speechOutput, SKILL_NAME, randomFact)
         this.emit(':tell', 'get protective gear to do');
     },
+    'ChemicalLookupIntent': function () {
+        chemicalLookup("water", "boiling_point");
+        this.emit(':tell', 'get protective gear to do');
+    },
     'AMAZON.HelpIntent': function () {
-        var speechOutput = HELP_MESSAGE;
-        var reprompt = HELP_REPROMPT;
+        const speechOutput = HELP_MESSAGE;
+        const reprompt = HELP_REPROMPT;
         this.emit(':ask', speechOutput, reprompt);
     },
     'AMAZON.CancelIntent': function () {
