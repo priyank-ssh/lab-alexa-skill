01fdf09c7105056be7516e16ffac2a0adf383f87
Zach Goldstein <zgoldstein@google.com>
2017-05-19 16:28:36 -0400

    Tabbing protocol messages.

--- src/protocolMapping.js
+++ src/protocolMapping.js
@@ -11,15 +11,32 @@ exports.cocaine = {
 };
 
 exports.abeiticAcid = {
-  "eye": "Immediately flush the eyes with plenty of WARM water for at least 15 minutes. Warm water MUST be used. If contact lenses are in the eyes, remove them. Get medical attention as soon as possible.",
-  "skin": "Immediately wash the affected area with soap and water. Then cover it with an emollient. Be sure to get medical attention if irritation develops.",
-  "mouth": "Do NOT induce vomiting unless directed to do so by medical personnel. If large quantities of this material are swallowed, call a physician immediately. If unconscious, don't try to give anything by mouth. Lastly, be sure to loosen tight clothing such as a collar, tie, belt or waistband."
+  "eye": "Immediately flush the eyes with plenty of WARM " +
+      "water for at least 15 minutes. Warm water MUST be used. " +
+      "If contact lenses are in the eyes, remove them. " +
+      "Get medical attention as soon as possible.",
+  "skin": "Immediately wash the affected area with soap and water. " +
+      "Then cover it with an emollient. Be sure to get medical attention " +
+      "if irritation develops.",
+  "mouth": "Do NOT induce vomiting unless directed to do so by medical personnel. " +
+      "If large quantities of this material are swallowed, call a physician immediately. " +
+      "If unconscious, don't try to give anything by mouth. " +
+      "Lastly, be sure to loosen tight clothing such as a collar, " +
+      "tie, belt or waistband."
 };
 
 exports.sodiumPhosphate = {
-  "eye": "Immediately flush the eyes with plenty of WARM water for at least 15 minutes. Warm water MUST be used. If contact lenses are in the eyes, remove them. Get medical attention as soon as possible.",
-  "skin": "Immediately wash the affected area with soap and water. Then cover it with an emollient. Be sure to get medical attention if irritation develops.",
-  "mouth": "Do NOT induce vomiting unless directed to do so by medical personnel. If large quantities of this material are swallowed, call a physician immediately. If unconscious, don't try to give anything by mouth. Lastly, be sure to loosen tight clothing such as a collar, tie, belt or waistband."
+  "eye": "Immediately flush the eyes with plenty of WARM water for at " +
+      "least 15 minutes. Warm water MUST be used. If contact lenses are in the eyes, " +
+      "remove them. Get medical attention as soon as possible.",
+  "skin": "Immediately wash the affected area with soap and water. " +
+      "Then cover it with an emollient. Be sure to get medical attention " +
+      "if irritation develops.",
+  "mouth": "Do NOT induce vomiting unless directed to do so by medical " +
+      "personnel. If large quantities of this material are swallowed, " +
+      "call a physician immediately. If unconscious, don't try to " +
+      "give anything by mouth. Lastly, be sure to loosen tight clothing such " +
+      "as a collar, tie, belt or waistband."
 };
 
 exports.trizmaBase = {
