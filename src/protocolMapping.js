d4488619afc02f165db224deb4fada7ba209aa60
Zach Goldstein <zgoldstein@google.com>
2017-05-19 16:35:18 -0400

    variables for harards.

--- src/protocolMapping.js
+++ src/protocolMapping.js
@@ -1,70 +1,69 @@
+const eye = "Immediately flush the eyes with plenty of WARM " +
+    "water for at least 15 minutes. Warm water MUST be used. " +
+    "If contact lenses are in the eyes, remove them. " +
+    "Get medical attention as soon as possible.";
+
+const skin = "Immediately wash the affected area with soap and water. " +
+    "Then cover it with an emollient. Be sure to get medical attention " +
+    "if irritation develops.";
+
+const mouth = "Do NOT induce vomiting unless directed to do so by medical personnel. " +
+    "If large quantities of this material are swallowed, call a physician immediately. " +
+    "If unconscious, don't try to give anything by mouth. " +
+    "Lastly, be sure to loosen tight clothing such as a collar, " +
+    "tie, belt or waistband.";
+
+
 exports.cocaineHydrochloride = {
-    "eye": "Immediately flush the eyes with plenty of WARM water for at least 15 minutes. Warm water MUST be used. If contact lenses are in the eyes, remove them. Get medical attention as soon as possible.",
-    "skin": "Immediately wash the affected area with soap and water. Then cover it with an emollient. Be sure to get medical attention if irritation develops.",
-    "mouth": "Do NOT induce vomiting unless directed to do so by medical personnel. If large quantities of this material are swallowed, call a physician immediately. If unconscious, don't try to give anything by mouth. Lastly, be sure to loosen tight clothing such as a collar, tie, belt or waistband."
+    "eye": eye,
+    "skin": skin,
+    "mouth": mouth
 };
 
 exports.cocaine = {
-    "eye": "Immediately flush the eyes with plenty of WARM water for at least 15 minutes. Warm water MUST be used. If contact lenses are in the eyes, remove them. Get medical attention as soon as possible.",
-    "skin": "Immediately wash the affected area with soap and water. Then cover it with an emollient. Be sure to get medical attention if irritation develops.",
-    "mouth": "Do NOT induce vomiting unless directed to do so by medical personnel. If large quantities of this material are swallowed, call a physician immediately. If unconscious, don't try to give anything by mouth. Lastly, be sure to loosen tight clothing such as a collar, tie, belt or waistband."
+    "eye": eye,
+    "skin": skin,
+    "mouth": mouth
 };
 
 exports.abeiticAcid = {
-  "eye": "Immediately flush the eyes with plenty of WARM " +
-      "water for at least 15 minutes. Warm water MUST be used. " +
-      "If contact lenses are in the eyes, remove them. " +
-      "Get medical attention as soon as possible.",
-  "skin": "Immediately wash the affected area with soap and water. " +
-      "Then cover it with an emollient. Be sure to get medical attention " +
-      "if irritation develops.",
-  "mouth": "Do NOT induce vomiting unless directed to do so by medical personnel. " +
-      "If large quantities of this material are swallowed, call a physician immediately. " +
-      "If unconscious, don't try to give anything by mouth. " +
-      "Lastly, be sure to loosen tight clothing such as a collar, " +
-      "tie, belt or waistband."
+  "eye": eye,
+  "skin": skin,
+  "mouth": mouth
 };
 
 exports.sodiumPhosphate = {
-  "eye": "Immediately flush the eyes with plenty of WARM water for at " +
-      "least 15 minutes. Warm water MUST be used. If contact lenses are in the eyes, " +
-      "remove them. Get medical attention as soon as possible.",
-  "skin": "Immediately wash the affected area with soap and water. " +
-      "Then cover it with an emollient. Be sure to get medical attention " +
-      "if irritation develops.",
-  "mouth": "Do NOT induce vomiting unless directed to do so by medical " +
-      "personnel. If large quantities of this material are swallowed, " +
-      "call a physician immediately. If unconscious, don't try to " +
-      "give anything by mouth. Lastly, be sure to loosen tight clothing such " +
-      "as a collar, tie, belt or waistband."
+    "eye": eye,
+    "skin": skin,
+    "mouth": mouth
 };
 
 exports.trizmaBase = {
-  "eye": "Immediately flush the eyes with plenty of WARM water for at least 15 minutes. Warm water MUST be used. If contact lenses are in the eyes, remove them. Get medical attention as soon as possible.",
-  "skin": "Immediately wash the affected area with soap and water. Then cover it with an emollient. Be sure to get medical attention if irritation develops.",
-  "mouth": "Do NOT induce vomiting unless directed to do so by medical personnel. If large quantities of this material are swallowed, call a physician immediately. If unconscious, don't try to give anything by mouth. Lastly, be sure to loosen tight clothing such as a collar, tie, belt or waistband."
+    "eye": eye,
+    "skin": skin,
+    "mouth": mouth
 };
 
 exports.dichlorophene = {
-  "eye": "Immediately flush the eyes with plenty of WARM water for at least 15 minutes. Warm water MUST be used. If contact lenses are in the eyes, remove them. Get medical attention as soon as possible.",
-  "skin": "Immediately wash the affected area with soap and water. Then cover it with an emollient. Be sure to get medical attention if irritation develops.",
-  "mouth": "Do NOT induce vomiting unless directed to do so by medical personnel. If large quantities of this material are swallowed, call a physician immediately. If unconscious, don't try to give anything by mouth. Lastly, be sure to loosen tight clothing such as a collar, tie, belt or waistband."
+    "eye": eye,
+    "skin": skin,
+    "mouth": mouth
 };
 
 exports.chloromethylMethylSulfide = {
-  "eye": "Immediately flush the eyes with plenty of WARM water for at least 15 minutes. Warm water MUST be used. If contact lenses are in the eyes, remove them. Get medical attention as soon as possible.",
-  "skin": "Immediately wash the affected area with soap and water. Then cover it with an emollient. Be sure to get medical attention if irritation develops.",
-  "mouth": "Do NOT induce vomiting unless directed to do so by medical personnel. If large quantities of this material are swallowed, call a physician immediately. If unconscious, don't try to give anything by mouth. Lastly, be sure to loosen tight clothing such as a collar, tie, belt or waistband."
+    "eye": eye,
+    "skin": skin,
+    "mouth": mouth
 };
 
 exports.phenylCyanateSolution = {
-  "eye": "Immediately flush the eyes with plenty of WARM water for at least 15 minutes. Warm water MUST be used. If contact lenses are in the eyes, remove them. Get medical attention as soon as possible.",
-  "skin": "Immediately wash the affected area with soap and water. Then cover it with an emollient. Be sure to get medical attention if irritation develops.",
-  "mouth": "Do NOT induce vomiting unless directed to do so by medical personnel. If large quantities of this material are swallowed, call a physician immediately. If unconscious, don't try to give anything by mouth. Lastly, be sure to loosen tight clothing such as a collar, tie, belt or waistband."
+    "eye": eye,
+    "skin": skin,
+    "mouth": mouth
 };
 
 exports.hydrogenBromide = {
-  "eye": "Immediately flush the eyes with plenty of WARM water for at least 15 minutes. Warm water MUST be used. If contact lenses are in the eyes, remove them. Get medical attention as soon as possible.",
-  "skin": "Immediately wash the affected area with soap and water. Then cover it with an emollient. Be sure to get medical attention if irritation develops.",
-  "mouth": "Do NOT induce vomiting unless directed to do so by medical personnel. If large quantities of this material are swallowed, call a physician immediately. If unconscious, don't try to give anything by mouth. Lastly, be sure to loosen tight clothing such as a collar, tie, belt or waistband."
+    "eye": eye,
+    "skin": skin,
+    "mouth": mouth
 };
