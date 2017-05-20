733e9c3cc647c1e245fe6939035e99273167a8ff
Jon Wicks <jwicks@hugeinc.com>
2017-05-20 10:56:30 -0400

    Fix fallback

--- src/propertyIntent.js
+++ src/propertyIntent.js
@@ -5,8 +5,8 @@ const propertyClient = require('./propertyClient');
 function getProductProperty(product, property, callback) {
     const productID = slotMapping.productNameToProductId[product];
     const propertyKey = slotMapping.propertyToKey[property];
-    propertyClient.getAllProductProperties(productID, callback);
-        
+    propertyClient.getProductProperty(productID, propertyKey, callback);
+
 }
 
 function getAllProductProperties(product, callback) {
